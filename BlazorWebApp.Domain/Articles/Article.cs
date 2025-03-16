1edd9e5b35b42693955ff51487b0f1e91fbcad9a
Philip Tomlinson <phil@philt.dev>
2025-03-16 20:43:58 +0100

    Add Article entity.

---
+++ BlazorWebApp.Domain/Articles/Article.cs
@@ -0,0 +1,10 @@
+﻿namespace BlazorWebApp.Domain.Articles;
+
+public class Article
+{
+    public int Id { get; set; }
+    public required string Title { get; set; }
+    public string? Content { get; set; }
+    public DateTime DatePublished { get; set; } = DateTime.Now;
+    public bool IsPublished { get; set; } = false;
+}
